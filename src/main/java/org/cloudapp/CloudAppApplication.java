9fe6b6d8771e5ad9e7f392e0fb2abddd602e69c0
gabriel-salmoria <gabriel.salmoria@proton.me>
2025-04-29 22:08:20 -0300

    mudando a tela de loginzinho e alguns fixes

--- src/main/java/org/cloudapp/CloudAppApplication.java
+++ src/main/java/org/cloudapp/CloudAppApplication.java
@@ -1,19 +1,43 @@
 package org.cloudapp;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 public class CloudAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CloudAppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CloudAppApplication.class, args);
+    }
 
-	@PostConstruct
-	public void init() {
-		System.out.println("CloudApp is running! Welcome!");
-	}
+    @PostConstruct
+    public void init() {
+        System.out.println("CloudApp is starting..."); // Mensagem inicial
+
+        char[] spinner = { '\\', '|', '/', '-' };
+        int numIterations = 50;
+        long delay = 400;
+
+        for (int i = 0; i < numIterations; i++) { // Loop para a animação
+            int spinnerIndex = i % spinner.length;
+            System.out.print("\r" + spinner[spinnerIndex] + " running! yay");
+
+            try {
+                Thread.sleep(delay);
+            } catch (InterruptedException e) {
+                break;
+            }
+        }
+
+        System.out.print("\r");
+        System.out.println("CloudApp is running! Welcome!");
+    }
+
+    public void start() {
+        System.out.println("CloudApp is running! Welcome!");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Iteration " + i);
+        }
+    }
 }
